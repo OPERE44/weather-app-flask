6624667004089b093d69012fb99b3b84ec33eaf5
OperePeter <peteropere3@gmail.com>
2022-06-16 15:45:23 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+  
+@app.route('/', methods =["GET", "POST"])
+def index(): 
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":       
+        cityName = request.form.get("cityName")  
+        if cityName:
+            weatherApiKey = 'aaf2d7c3852f4083d6de55ab474ed655'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+            print(url)
+        else:
+            error = 1    
+    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
+if __name__ == "__main__":
+    app.run(debug=True)
